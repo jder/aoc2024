b04b52a73d54db566d85f823bf5934d22ba282bc
Jesse Rusak <me@jesserusak.com>
2024-12-01 09:14:45 -0500

    day 1 part 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,5 +1,25 @@
+use itertools::Itertools;
 use std::fs;
 
 fn main() {
-    println!("{}", fs::read_to_string("input/day1.txt").unwrap());
+    let (mut first, mut second): (Vec<i32>, Vec<i32>) = fs::read_to_string("input/day1.txt")
+        .unwrap()
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .unzip();
+
+    first.sort();
+    second.sort();
+
+    let total = first
+        .into_iter()
+        .zip(second)
+        .map(|(f, s)| (f - s).abs())
+        .sum::<i32>();
+    println!("{}", total);
 }
