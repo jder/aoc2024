19a9040fe405438a78c960b27ff402d264c3f854
Jesse Rusak <me@jesserusak.com>
2024-12-02 12:12:10 -0500

    day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 use crate::prelude::*;
 
 fn report_safe(levels: &[i32]) -> bool {
@@ -6,18 +8,36 @@ fn report_safe(levels: &[i32]) -> bool {
         || diffs.iter().all(|diff| [1, 2, 3].contains(diff))
 }
 
-pub fn part1(input: &str) -> usize {
-    let reports: Vec<Vec<i32>> = input
+fn parse_reports(input: &str) -> Vec<Vec<i32>> {
+    input
         .lines()
         .map(|line| {
             line.split_ascii_whitespace()
                 .map(|x| x.parse::<i32>().unwrap())
                 .collect()
         })
-        .collect();
+        .collect()
+}
+
+pub fn part1(input: &str) -> usize {
+    let reports = parse_reports(input);
     reports.iter().filter(|levels| report_safe(&levels)).count()
 }
 
-pub fn part2(input: &str) -> i32 {
-    todo!()
+fn mutated_report_safe(levels: &[i32]) -> bool {
+    iter::once(levels.to_vec())
+        .chain((0..levels.len()).map(|i| {
+            let mut levels = levels.to_vec();
+            levels.remove(i);
+            levels
+        }))
+        .any(|levels| report_safe(&levels))
+}
+
+pub fn part2(input: &str) -> usize {
+    let reports = parse_reports(input);
+    reports
+        .iter()
+        .filter(|levels| mutated_report_safe(&levels))
+        .count()
 }
