05ea53b0ec31814f5d0e2c17db9d877f78595e35
Jesse Rusak <me@jesserusak.com>
2024-12-03 07:50:07 -0500

    day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -14,5 +14,25 @@ pub fn part1(input: &str) -> usize {
 }
 
 pub fn part2(input: &str) -> usize {
-    todo!()
+    let mut enabled = true;
+    regex::Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
+        .unwrap()
+        .captures_iter(input)
+        .map(|m| match m.get(0).unwrap().as_str() {
+            "do()" => {
+                enabled = true;
+                0
+            }
+            "don't()" => {
+                enabled = false;
+                0
+            }
+            _ if enabled => m
+                .iter()
+                .skip(1)
+                .map(|x| x.unwrap().as_str().parse::<usize>().unwrap())
+                .product::<usize>(),
+            _ => 0,
+        })
+        .sum()
 }
