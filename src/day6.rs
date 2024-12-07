2e02cea05c755645eea721d6fe2fe21c8bfdb505
Jesse Rusak <me@jesserusak.com>
2024-12-06 20:29:19 -0500

    avoid lots of hashing

--- src/day6.rs
+++ src/day6.rs
@@ -1,27 +1,107 @@
 use std::collections::{hash_set, HashSet};
 
+use bitvec::vec::BitVec;
+
 use crate::prelude::*;
 
+#[repr(isize)]
+#[derive(Clone, Copy)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+impl Direction {
+    pub fn turn_right(&self) -> Self {
+        match self {
+            Self::Up => Self::Right,
+            Self::Right => Self::Down,
+            Self::Down => Self::Left,
+            Self::Left => Self::Up,
+        }
+    }
+
+    pub fn heading(&self) -> (Index, Index) {
+        match self {
+            Self::Up => (0, -1),
+            Self::Right => (1, 0),
+            Self::Down => (0, 1),
+            Self::Left => (-1, 0),
+        }
+    }
+}
+
+struct Visited {
+    width: usize,
+    height: usize,
+    bitvec: BitVec,
+}
+
+impl Visited {
+    fn new(width: usize, height: usize) -> Self {
+        Self {
+            width: width,
+            height: height,
+            bitvec: BitVec::repeat(false, width * height * 4),
+        }
+    }
+
+    fn insert(&mut self, location: Location, heading: Direction) -> bool {
+        let index = (location.y as usize * self.width + location.x as usize) * 4 + heading as usize;
+        let was_set = *self.bitvec.get(index).unwrap();
+        self.bitvec.set(index, true);
+        was_set
+    }
+
+    fn iter(&self) -> impl Iterator<Item = (Location, Direction)> + '_ {
+        self.bitvec
+            .iter()
+            .enumerate()
+            .filter_map(move |(index, set)| {
+                if *set {
+                    let y = index / (self.width * 4);
+                    let x = (index % (self.width * 4)) / 4;
+                    let heading = index % 4;
+                    Some((
+                        Location {
+                            x: x as Index,
+                            y: y as Index,
+                        },
+                        match heading {
+                            0 => Direction::Up,
+                            1 => Direction::Right,
+                            2 => Direction::Down,
+                            3 => Direction::Left,
+                            _ => unreachable!(),
+                        },
+                    ))
+                } else {
+                    None
+                }
+            })
+    }
+}
+
 pub fn walk(map: &Grid<char>) -> Option<usize> {
-    let mut visited = HashSet::new();
+    let mut visited = Visited::new(map.width(), map.height());
 
     let mut now = map.cells().find(|cell| *cell.contents() == '^').unwrap();
-    let mut heading = (0, -1);
+    let mut direction = Direction::Up;
     loop {
-        let current_state = (now.location(), heading);
-        match visited.entry(current_state) {
-            hash_set::Entry::Occupied(_) => break None,
-            hash_set::Entry::Vacant(entry) => entry.insert(),
+        if visited.insert(now.location(), direction) {
+            break None;
         }
-        if let Some(in_front) = now.offset(heading.0, heading.1) {
+        if let Some(in_front) = now.offset(direction.heading().0, direction.heading().1) {
             if *in_front.contents() == '#' {
                 // turn 90 degrees to the right
-                heading = (-heading.1, heading.0);
+                direction = direction.turn_right();
             } else {
                 now = in_front;
             }
         } else {
-            break Some(visited.into_iter().unique_by(|v| v.0).count());
+            break Some(visited.iter().unique_by(|v| v.0).count());
         }
     }
 }
