3a1345ed6d3cf6e23f77abc91fc010b533d14f87
Jesse Rusak <me@jesserusak.com>
2024-12-08 11:47:01 -0500

    cleanups

--- src/day8.rs
+++ src/day8.rs
@@ -6,23 +6,19 @@ use crate::prelude::*;
 
 pub fn part1(input: &str) -> usize {
     let map = Grid::new_with_lines(input.lines());
-    map.cells()
-        .filter(|c| *c.contents() != '.')
-        .cartesian_product(map.cells())
-        .filter_map(|(a, b)| {
-            if a == b {
-                return None;
-            }
+    let stations = map.cells().filter(|c| *c.contents() != '.');
 
-            if a.contents() == b.contents() {
+    stations
+        .clone()
+        .cartesian_product(stations)
+        .filter_map(|(a, b)| {
+            if a != b && a.contents() == b.contents() {
                 let a_to_b = b.location() - a.location();
                 let twice_b = a.location() + a_to_b * 2;
-                if map.cell(twice_b).is_some() {
-                    return Some(twice_b);
-                }
+                map.cell(twice_b)
+            } else {
+                None
             }
-
-            None
         })
         .unique()
         .count()
@@ -30,29 +26,23 @@ pub fn part1(input: &str) -> usize {
 
 pub fn part2(input: &str) -> usize {
     let map = Grid::new_with_lines(input.lines());
-    map.cells()
-        .filter(|c| *c.contents() != '.')
-        .cartesian_product(map.cells())
-        .flat_map(|(a, b)| {
-            if a == b {
-                return Either::Left(iter::empty());
-            }
+    let stations = map.cells().filter(|c| *c.contents() != '.');
 
-            if a.contents() == b.contents() {
+    stations
+        .clone()
+        .cartesian_product(stations)
+        .flat_map(|(a, b)| {
+            if a != b && a.contents() == b.contents() {
                 let a_to_b = b.location() - a.location();
                 let mut now = a.location();
                 let map = &map;
-                return Either::Right(iter::from_fn(move || {
+                Either::Left(iter::from_fn(move || {
                     now += a_to_b;
-                    if map.cell(now).is_some() {
-                        Some(now)
-                    } else {
-                        None
-                    }
-                }));
+                    map.cell(now)
+                }))
+            } else {
+                Either::Right(iter::empty())
             }
-
-            Either::Left(iter::empty())
         })
         .unique()
         .count()
