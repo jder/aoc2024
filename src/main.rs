865c7c59f78e7eee951a3e0fd49fd5f38e369866
Jesse Rusak <me@jesserusak.com>
2024-12-14 11:56:30 -0500

    finally move to day0x

--- src/main.rs
+++ src/main.rs
@@ -108,34 +108,34 @@ struct Args {
     sample: bool,
 }
 
-mod day1;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
 mod day10;
 mod day11;
 mod day12;
 mod day13;
 mod day14;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
 
 pub fn main() {
     env_logger::init();
 
     let mut runner = Runner::new();
-    runner.register_day("day1", day1::part1, day1::part2);
-    runner.register_day("day2", day2::part1, day2::part2);
-    runner.register_day("day3", day3::part1, day3::part2);
-    runner.register_day("day4", day4::part1, day4::part2);
-    runner.register_day("day5", day5::part1, day5::part2);
-    runner.register_day("day6", day6::part1, day6::part2);
-    runner.register_day("day7", day7::part1, day7::part2);
-    runner.register_day("day8", day8::part1, day8::part2);
-    runner.register_day("day9", day9::part1, day9::part2);
+    runner.register_day("day01", day01::part1, day01::part2);
+    runner.register_day("day02", day02::part1, day02::part2);
+    runner.register_day("day03", day03::part1, day03::part2);
+    runner.register_day("day04", day04::part1, day04::part2);
+    runner.register_day("day05", day05::part1, day05::part2);
+    runner.register_day("day06", day06::part1, day06::part2);
+    runner.register_day("day07", day07::part1, day07::part2);
+    runner.register_day("day08", day08::part1, day08::part2);
+    runner.register_day("day09", day09::part1, day09::part2);
     runner.register_day("day10", day10::part1, day10::part2);
     runner.register_day("day11", day11::part1, day11::part2);
     runner.register_day("day12", day12::part1, day12::part2);
