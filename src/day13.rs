afc0928ee1617e367ce2ced922a13537bc0a802c
Jesse Rusak <me@jesserusak.com>
2024-12-13 21:26:26 -0500

    day 13

---
+++ src/day13.rs
@@ -0,0 +1,68 @@
+use crate::prelude::*;
+
+type Num = i64;
+
+#[derive(Debug)]
+struct Game {
+    goal: (Num, Num),
+    a: (Num, Num),
+    b: (Num, Num),
+}
+
+fn parse(input: &str) -> Vec<Game> {
+    let integer = regex::Regex::new(r"(\d+)").unwrap();
+
+    input
+        .split("\n\n")
+        .map(|game| {
+            let (a, b, goal) = game
+                .lines()
+                .map(|line| {
+                    integer
+                        .find_iter(line)
+                        .map(|x| x.as_str().parse::<Num>().unwrap())
+                        .collect_tuple::<(Num, Num)>()
+                        .unwrap()
+                })
+                .collect_tuple()
+                .unwrap();
+            Game { goal, a, b }
+        })
+        .collect()
+}
+
+fn solve_game(Game { goal, a, b }: Game) -> Option<Num> {
+    let b_count = (goal.0 * a.1 - goal.1 * a.0) / (b.0 * a.1 - b.1 * a.0);
+    let a_count = (goal.0 - b.0 * b_count) / a.0;
+
+    if a_count * a.0 + b_count * b.0 == goal.0 && a_count * a.1 + b_count * b.1 == goal.1 {
+        Some(3 * a_count + b_count)
+    } else {
+        None
+    }
+}
+
+pub fn part1(input: &str) -> Num {
+    let games = parse(input);
+    games.into_iter().filter_map(solve_game).sum()
+}
+
+pub fn part2(input: &str) -> Num {
+    let games = parse(input);
+    games
+        .into_iter()
+        .flat_map(
+            |Game {
+                 goal: orig_goal,
+                 a,
+                 b,
+             }| {
+                solve_game(Game {
+                    goal: (orig_goal.0 + 10000000000000, orig_goal.1 + 10000000000000),
+                    a,
+                    b,
+                })
+            },
+        )
+        .sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -112,6 +112,7 @@ mod day1;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 mod day2;
 mod day3;
 mod day4;
@@ -135,6 +136,7 @@ pub fn main() {
     runner.register_day("day10", day10::part1, day10::part2);
     runner.register_day("day11", day11::part1, day11::part2);
     runner.register_day("day12", day12::part1, day12::part2);
+    runner.register_day("day13", day13::part1, day13::part2);
 
     let args = Args::parse();
 
